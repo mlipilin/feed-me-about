a383d567e745fecd5792ff0e4d7d0da246822187
Mikhail Lipilin <mikhail.lipilin@balance-pl.ru>
2021-10-09 22:32:04 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+window.renderAbout = (containerId) => {
+  console.log('renderAbout', { containerId })
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById(containerId)
+  )
+  reportWebVitals()
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+window.unmountAbout = (containerId) => {
+  console.log('unmountAbout', { containerId })
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId))
+}
